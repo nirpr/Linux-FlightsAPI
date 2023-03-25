e359552a3b07e7f39d69777fb96eb717f61ede20
nirpr <nirpr@mta.ac.il>
2023-03-25 21:53:33 +0300

    just made sure that the flightDB folder will be created if not already and add in # some of the commands to change the json file to csv

--- flightScanner.sh
+++ flightScanner.sh
@@ -7,10 +7,15 @@ echo "end time is $current_time"
 echo "Get last 24hours flights into flightsDB"
 for airport in "$@" 
 do
-    mkdir "flightsDB/$airport"
+    mkdir -p "flightsDB/$airport"
     echo "curl -s \"https://opensky-network.org/api/flights/arrival?airport=$airport&begin=$time_before_day&end=$current_time\""
     curl -s "https://opensky-network.org/api/flights/arrival?airport=$airport&begin=$time_before_day&end=$current_time" | sed 's/},{/},\n{/g' | cat > "flightsDB/$airport/$airport.arv"
     echo "curl -s \"https://opensky-network.org/api/flights/departure?airport=$airport&begin=$time_before_day&end=$current_time\""
     curl -s "https://opensky-network.org/api/flights/departure?airport=$airport&begin=$time_before_day&end=$current_time" | sed 's/},{/},\n{/g' | cat > "flightsDB/$airport/$airport.dst"
 done
 echo "Done"
+
+
+
+# cat LLBG.arv | sed 's/\[//;s/\]//' | awk -F '[,:}]' '{print $2","$4","$6","$8","$10","$12","$16","$18","$21","$24","$27}' | sed 's/"/ /g'
+# so far for updating the json file
