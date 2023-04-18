fdc7044495ae9b165cb95269e4ce1fa849841eab
OmerKr <omerkriger@gmail.com>
2023-04-18 20:28:55 +0300

    - add loadDB to this programs

--- lib/arrivals.cpp
+++ lib/arrivals.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include "flightDB.h"
 using namespace std;
 
 int main(int argc, char **argv)
 {
     if(argc <= 1) // print error for didn't send argument to command
         cerr << "Error: arguments didn't send." << endl;
-
+    FlightDatabase DB;
+    string pathDB(argv[0]);
+    pathDB += "/flightsDB"; // adding flightsDB directory
+    for(int i=1; i < argc;i++)
+        DB.load_db(pathDB, argv[i]); 
 }
