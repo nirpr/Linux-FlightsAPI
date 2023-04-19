ac014eb4e6b9c6930401dabca601054b53e004e5
nirpr <nirpr@mta.ac.il>
2023-04-19 14:26:29 +0300

    updates the arv and dst files and now the scripts handles errors as well

--- flightScanner.sh
+++ flightScanner.sh
@@ -2,26 +2,54 @@
 
 # TODO: Need to add error handle examples: no curl apt, curl return [] empty, error in sed to awk commands
 
-# -- Writed by Omer Start --
+# -- Written by Omer Start --
+if [ $# -eq 0 ]; then
+    echo "Error: No airport codes provided"
+    exit 1
+fi
 current_time=$(date +%s)
 time_before_day=$(expr $current_time - 86400)  # 86400 - 24 hours
 echo "Get last 24hours flights into flightsDB"
 for airport in "$@" 
 do
+    if ! curl -s --fail "https://opensky-network.org/api/flights/arrival?airport=$airport&begin=$time_before_day&end=$current_time" >/dev/null; then
+    echo "Error: Invalid airport code - $airport"
+    continue
+    fi
+    if ! curl -s --fail "https://opensky-network.org/api/flights/departure?airport=$airport&begin=$time_before_day&end=$current_time" >/dev/null; then
+    echo "Error: Invalid airport code - $airport"
+    continue
+    fi
+
     mkdir -p "flightsDB/$airport"
     curl -s "https://opensky-network.org/api/flights/arrival?airport=$airport&begin=$time_before_day&end=$current_time" | sed 's/},{/},\n{/g' | cat > "flightsDB/$airport/$airport.arv"
     curl -s "https://opensky-network.org/api/flights/departure?airport=$airport&begin=$time_before_day&end=$current_time" | sed 's/},{/},\n{/g' | cat > "flightsDB/$airport/$airport.dst"
 
-    # --- Writed by Nir Start ---
-    keys=$(cat flightsDB/$airport/$airport.arv | awk -F'[:,]' 'BEGIN{ORS=""} {for(i=1;i<=NF;i+=2) if ($i in seen) next; else {seen[$i]; printf "%s%s", $i, (i==NF-1) ? "" : ","}} END{print "\n"}' | sed 's/[][]//g' | sed 's/,{"icao24",//' | sed 's/,$//' | tr -d '"' | sed 's/^{//')
-    values=$(cat flightsDB/$airport/$airport.arv  | sed 's/\[//;s/\]//' | awk -F '[,:}]' '{print $2","$4","$6","$8","$10","$12","$14","$16","$18","$20","$22","$24}' | sed 's/"/ /g' | sed 's/ //g')
-    echo "$keys"  > flightsDB/$airport/$airport.arv                                                                                                                                                      
-    echo "$values" >> flightsDB/$airport/$airport.arv
-    keys=$(cat flightsDB/$airport/$airport.dst | awk -F'[:,]' 'BEGIN{ORS=""} {for(i=1;i<=NF;i+=2) if ($i in seen) next; else {seen[$i]; printf "%s%s", $i, (i==NF-1) ? "" : ","}} END{print "\n"}' | sed 's/[][]//g' | sed 's/,{"icao24",//' | sed 's/,$//' | tr -d '"' | sed 's/^{//')
-    values=$(cat flightsDB/$airport/$airport.dst  | sed 's/\[//;s/\]//' | awk -F '[,:}]' '{print $2","$4","$6","$8","$10","$12","$14","$16","$18","$20","$22","$24}' | sed 's/"/ /g' | sed 's/ //g')
-    echo "$keys"  > flightsDB/$airport/$airport.dst                                                                                                                                                      
-    echo "$values" >> flightsDB/$airport/$airport.dst
-    # --- Writed by Nir End ---
+    # --- Written by Nir Start ---
+    #arv file manipulation:
+    keys=$(cat flightsDB/$airport/$airport.arv | awk -F'[:,]' '{print $1, $3, $5, $7, $9, $11}' | sort | uniq | head -1 | sed -e 's/"//g' -e 's/[{[]//g' -e 's/]/,/g' -e 's/ /,/g')
+    values=$(cat flightsDB/$airport/$airport.arv  | sed 's/\[//;s/\]//' | awk -F '[,:}]' '{print $2","$4","$6","$8","$10","$12}' | sed 's/"/ /g' | sed 's/ //g')
+    echo -e "$keys\n$values" > flightsDB/$airport/$airport.arv
+    #check if file empty and then delete if empty
+    if grep -q "^[,]*$" "flightsDB/$airport/$airport.arv"; then
+    echo "No arrivals found for $airport in the last 24 hours."
+    rm flightsDB/$airport/$airport.arv
+    fi
+    #dst file manipulation:
+    keys=$(cat flightsDB/$airport/$airport.dst | awk -F'[:,]' '{print $1, $3, $5, $7, $9, $11}' | sort | uniq | head -1 | sed -e 's/"//g' -e 's/[{[]//g' -e 's/]/,/g' -e 's/ /,/g')
+    values=$(cat flightsDB/$airport/$airport.dst  | sed 's/\[//;s/\]//' | awk -F '[,:}]' '{print $2","$4","$6","$8","$10","$12}' | sed 's/"/ /g' | sed 's/ //g')
+    echo -e "$keys\n$values" > flightsDB/$airport/$airport.dst
+    #check if file empty and then delete if empty
+    if grep -q "^[,]*$" "flightsDB/$airport/$airport.dst"; then
+    echo "No departures found for $airport in the last 24 hours."
+    rm flightsDB/$airport/$airport.dst
+    fi
+    #check if directory is empty and then delete if it is.
+    if [ -n "$(find flightsDB/$airport -maxdepth 0 -type d -empty)" ]; then
+    echo "There are no departures or arrivals to/from $airport, deleting directory."
+    rmdir "flightsDB/$airport"
+    fi
+    # --- Written by Nir End ---
 done
 echo "Done"
-# -- Writed by Omer END --
+# -- Written by Omer END --
